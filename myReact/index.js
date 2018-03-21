3707a2457c240e899ead76ead1022eb3c1876811
Hamuliak Nazar <ifuadev@gmail.com>
2018-03-21 20:26:38 +0200

    create: myReact

---
+++ myReact/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const React = {
+    createElement: function (tag, options = undefined, node = null) {
+        const element = document.createElement(tag);
+
+        Array.isArray(node) ? element.append(...node) : element.innerHTML = node;
+
+        // set attrs for element like style and textContent
+        (options && options.style) ? element.style.backgroundColor = options.style.backgroundColor : null;
+        (options && options.textContent) ? element.innerHTML = options.textContent : null;
+
+        return element;
+    },
+    render: function (view, root) {
+        root.appendChild(view);
+    }
+};
+
+const app =
+    React.createElement('div', { style: { backgroundColor: 'red' } }, [
+        React.createElement('span', undefined, 'Hello world'),
+        React.createElement('br'),
+        'This is just a text node',
+        React.createElement('div', { textContent: 'Text content' }),
+    ]);
+
+React.render(
+    app,
+    document.getElementById('root'),
+);
